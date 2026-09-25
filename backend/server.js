154341ee5902b238c13afeead25debdf64c7b2db
Devanshu Dubey <147015983+devanshu091104@users.noreply.github.com>
2026-09-25 19:11:11 +0000

    Remove unnecessary blank line in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const { testConnection } = require("./src/config/db");
 const {
   verifyEmailConnection,
 } = require("./src/services/email.service");
-
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
